c1587bdcdb9ea11582d0ac8fdb2cb6f395abe87b
Daniel Mouris <dgmouris@gmail.com>
2025-09-12 14:37:42 -0600

    class progress pushed

--- 2-review-astronauts-example-START/js/api/astronaut.js
+++ 2-review-astronauts-example-START/js/api/astronaut.js
@@ -1,3 +1,8 @@
 const BASE_URL = "https://lldev.thespacedevs.com/2.2.0"
 
 // api functions here.
+
+// we're going to create a function called
+// get astronaut list, which will fetch the astronaut
+// data from the docs here https://ll.thespacedevs.com/docs/#/astronauts
+
--- 2-review-astronauts-example-START/js/index.js
+++ 2-review-astronauts-example-START/js/index.js
@@ -1,1 +1,5 @@
+// the first thing I'm going to do here
+// which is common for javvascript bundlers
+// packaging is to import your css.
+import 'bootstrap/dist/css/bootstrap.min.css';
 // js here.
