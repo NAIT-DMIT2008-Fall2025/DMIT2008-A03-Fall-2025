b3554295d1023e8cf829ee156526f0c8e2a6f0e9
Daniel Mouris <dgmouris@gmail.com>
2025-09-12 14:46:59 -0600

    class progress pushed

--- 2-review-astronauts-example-START/js/api/astronaut.js
+++ 2-review-astronauts-example-START/js/api/astronaut.js
@@ -7,3 +7,17 @@ const BASE_URL = "https://lldev.thespacedevs.com/2.3.0"
 // get astronaut list, which will fetch the astronaut
 // data from the docs here https://ll.thespacedevs.com/docs/#/astronauts
 
+const getAstronautList = async () => {
+  // we're going to create the url
+  const URL = `${BASE_URL}/astronauts/?mode=list&search=canadian`
+  // make the fetch request
+  const response = await fetch(URL, {method: "GET"})
+  // let's get some json
+  const data = await response.json()
+
+  return data
+
+}
+
+// we're export this function
+export { getAstronautList }
