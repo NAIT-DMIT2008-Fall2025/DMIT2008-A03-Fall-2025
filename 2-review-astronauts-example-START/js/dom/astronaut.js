de649413249cb9db1c4a247911cdb626198b4256
Daniel Mouris <dgmouris@gmail.com>
2025-09-17 13:47:39 -0600

    class progress pushed

--- 2-review-astronauts-example-START/js/dom/astronaut.js
+++ 2-review-astronauts-example-START/js/dom/astronaut.js
@@ -4,11 +4,14 @@ replace the instances that have "THIS FORMAT HERE" with the astronautData.
 
 <li href="#" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
-        <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">    
+        <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">
         <h5 class="mb-1">ASTRONAUT NAME (ASTRONAUT STATUS HERE)</h5>
         <small class="float-end">born DATE OF BIRTH HERE</small>
     </div>
-    <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>    
+    <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>
     <p class="mb-1">ASTRONAUT BIO HERE </p>
 </li>
 */
+
+// we're going to create a function that is going to use some destructuring
+// to get some of this data.
--- 2-review-astronauts-example-START/js/index.js
+++ 2-review-astronauts-example-START/js/index.js
@@ -17,6 +17,16 @@ const app = async () => {
   const astronautData = await getAstronautList()
   console.log('astronaut data fetched')
   console.log(astronautData)
+  // I'm going to loop through each astronaut
+  console.log(Array.isArray(astronautData)) // is not an array it's an obj.
+  console.log(Array.isArray(astronautData.results)) // is  an array
+
+  astronautData.results.map((astronaut, index)=> {
+    // On the page I want to render each astronaut
+    console.log("Astronaut: " + index)
+    console.log(astronaut)
+
+  })
 }
 
 // a note here this is async but we're not using the result of the function
