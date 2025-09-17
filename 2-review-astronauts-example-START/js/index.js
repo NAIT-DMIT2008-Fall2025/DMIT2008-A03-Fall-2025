90f996e6ea9d3f317da08966918cc1fbd917ad32
Daniel Mouris <dgmouris@gmail.com>
2025-09-17 13:41:36 -0600

    class progress pushed

--- 2-review-astronauts-example-START/js/index.js
+++ 2-review-astronauts-example-START/js/index.js
@@ -2,7 +2,24 @@
 // which is common for javvascript bundlers
 // packaging is to import your css.
 import 'bootstrap/dist/css/bootstrap.min.css';
-// js here.
+
+// inside of the index.js we're going to import this.
+import { getAstronautList } from './api/astronaut';
 
 
-console.log('our astronauts app')
+
+const app = async () => {
+  console.log('our astronauts app')
+  // we're going to need to call our get astronaut list to get the data
+  // to populate on the page.
+  // call this function
+  // call our getAstronautData and console it out to the page.
+  const astronautData = await getAstronautList()
+  console.log('astronaut data fetched')
+  console.log(astronautData)
+}
+
+// a note here this is async but we're not using the result of the function
+// so we can just call it without the await
+
+app()
