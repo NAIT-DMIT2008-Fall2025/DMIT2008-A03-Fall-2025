cc668856803c02b8650b8b65f57d1887fd7b0152
Daniel Mouris <dgmouris@gmail.com>
2025-09-24 14:24:44 -0600

    class progress pushed

---
+++ 3-nextjs-intro-START/hello-next-dmit2008/components/LearningList.jsx
@@ -0,0 +1,6 @@
+export default function LearningList({title}) {
+
+    return <div>
+        <h2>things we've learned about: {title}</h2>
+    </div>
+}
--- 3-nextjs-intro-START/hello-next-dmit2008/pages/index.js
+++ 3-nextjs-intro-START/hello-next-dmit2008/pages/index.js
@@ -1,24 +1,12 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // we'll see this a bit later on but pages
 // in the app and pages router of next.js are
 // are also components.
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
+    <div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {/* Over here this is returning JSX */}
         <h1>Our First React App</h1>
