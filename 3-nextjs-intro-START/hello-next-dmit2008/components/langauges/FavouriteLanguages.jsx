538444632e5b862732be9cd7661655a6d14eec79
Daniel Mouris <dgmouris@gmail.com>
2025-09-26 14:38:46 -0600

    class progress pushed

--- 3-nextjs-intro-START/hello-next-dmit2008/components/langauges/FavouriteLanguages.jsx
+++ 3-nextjs-intro-START/hello-next-dmit2008/components/langauges/FavouriteLanguages.jsx
@@ -1,7 +1,19 @@
-export default function FavouriteLanaguages({ languages }) {
+// import the LanguageItem component
+import LanguageItem from "./LanguageItem"
+
+export default function FavouriteLanguages({ languages }) {
 
     // ordered list
     return <ol>
         {/* render the list item */}
+        {languages.map((lang, index) => {
+            // below where it says language={lang}
+            // lang is the actual list item
+            // language=... is the prop to the LanguageItemComponent
+            return <LanguageItem
+                key={index}
+                language={lang}
+            />
+        })}
     </ol>
 }
--- 3-nextjs-intro-START/hello-next-dmit2008/components/langauges/LanguageItem.jsx
+++ 3-nextjs-intro-START/hello-next-dmit2008/components/langauges/LanguageItem.jsx
@@ -1,6 +1,8 @@
 // the specific items we're going to loop over in the
 // FavouriteLanguages component.
 export default function LanguageItem({language}) {
+    // in html components in react we use className
+    // rather than class
     return <li className="text-blue-600">
         {language}
     </li>
