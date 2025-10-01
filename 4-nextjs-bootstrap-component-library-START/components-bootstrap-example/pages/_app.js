d71fa0e8be28b89b2ab38cd881f72760dd30f226
Daniel Mouris <dgmouris@gmail.com>
2025-10-01 14:05:48 -0600

    class progress pushed

--- 4-nextjs-bootstrap-component-library-START/components-bootstrap-example/pages/_app.js
+++ 4-nextjs-bootstrap-component-library-START/components-bootstrap-example/pages/_app.js
@@ -1,4 +1,8 @@
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
+
+// we're going to import the bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
