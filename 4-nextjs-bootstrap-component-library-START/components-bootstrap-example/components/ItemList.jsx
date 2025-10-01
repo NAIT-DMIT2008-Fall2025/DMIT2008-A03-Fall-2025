f0c7fe41b0b027159c797bd2751ccb379757d6ef
Daniel Mouris <dgmouris@gmail.com>
2025-10-01 14:35:02 -0600

    class progress pushed

--- 4-nextjs-bootstrap-component-library-START/components-bootstrap-example/components/ItemList.jsx
+++ 4-nextjs-bootstrap-component-library-START/components-bootstrap-example/components/ItemList.jsx
@@ -1,3 +1,6 @@
+// let's import the ListGroup from react
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 // because I want my component to be called like so in the jsx
 // <ItemList items={UI_LIBRARY} title={"Awesome libraries"} />
@@ -8,7 +11,33 @@ export default function ItemList({items, title}) {
     return <div>
         {/* I want to define a title i'll but that in a h2 */}
         <h2>{title}</h2>
+        {/* Let's create the list using react bootstrap */}
+        <ListGroup>
+            {/* we're going to have a list of items that
+                with map there's two options
+                option 1
+                items.map((item, index) => { return //...jsx here });
 
+                option 2 (implies the return)
+                items.map((item, index) => (//...jsx here));
+
+            */}
+
+            {items.map((item, index) => {
+                // item is an object
+                console.log(item)
+                // we're going to return a ListGroup.Item for each
+                return <ListGroup.Item
+                    key={index}
+                >
+                    {/* let's make this a link */}
+                    <a href={item.url} target="_blank">
+
+                        {item.name}
+                    </a>
+                </ListGroup.Item>
+            })}
+        </ListGroup>
     </div>
 
 }
