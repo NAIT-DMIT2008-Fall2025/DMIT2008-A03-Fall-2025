db36b9236f3e50da85e1b13b8dd4a992eb2d52bf
Daniel Mouris <dgmouris@gmail.com>
2025-10-03 13:42:30 -0600

    class progress pushed

--- 5-nextjs-mui-component-library-START/component-mui-example/pages/index.js
+++ 5-nextjs-mui-component-library-START/component-mui-example/pages/index.js
@@ -1,3 +1,9 @@
+
+// I want you to read the docs and see if you can add
+// a container
+// a grid
+// a typorgraphy component with a title that says MUI example
+
 // gut the index page
 export default function Home() {
   return (
