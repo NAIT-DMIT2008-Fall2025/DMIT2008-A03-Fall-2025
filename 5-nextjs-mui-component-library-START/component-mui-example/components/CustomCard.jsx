df973039bb2b79b9d434e91a721e06316e630850
Daniel Mouris <dgmouris@gmail.com>
2025-10-03 14:12:51 -0600

    class progress pushed

--- 5-nextjs-mui-component-library-START/component-mui-example/components/CustomCard.jsx
+++ 5-nextjs-mui-component-library-START/component-mui-example/components/CustomCard.jsx
@@ -1,5 +1,5 @@
 // create the component
-// three props
+// with three props
 // - a title
 // - body (which is going to take some jsx as the value)
 // - actions (which is also going to be some jsx)
