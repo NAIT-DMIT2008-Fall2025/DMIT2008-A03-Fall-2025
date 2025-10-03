0ddb84dc34e229c31ee720daf2caff5ccda4065f
Daniel Mouris <dgmouris@gmail.com>
2025-10-03 13:53:17 -0600

    class progress pushed

--- 5-nextjs-mui-component-library-START/component-mui-example/pages/index.js
+++ 5-nextjs-mui-component-library-START/component-mui-example/pages/index.js
@@ -1,15 +1,23 @@
 
 // I want you to read the docs and see if you can add
-// a container
-// a grid
+// a container: refer to docs here https://mui.com/material-ui/react-container/
+import Container from '@mui/material/Container';
+// a grid: https://mui.com/material-ui/react-grid/
+import Grid from '@mui/material/Grid';
 // a typography component with a title that says MUI example
 
 
 // gut the index page
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container maxWidth="lg">
+
+      <Grid container spacing={2}>
+        <Grid size={12}>
+          {/* we're going to put the title here */}
+          <h1>Home</h1>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
