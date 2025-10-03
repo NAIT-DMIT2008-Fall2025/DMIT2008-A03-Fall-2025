ee8ceb1882c59d9a1bd8fda43248d91e39365fda
Daniel Mouris <dgmouris@gmail.com>
2025-10-03 14:13:45 -0600

    class progress pushed

--- 5-nextjs-mui-component-library-START/component-mui-example/components/CustomCard.jsx
+++ 5-nextjs-mui-component-library-START/component-mui-example/components/CustomCard.jsx
@@ -1,6 +1,7 @@
-// create the component
+// create the component docs
 // with three props
 // - a title
 // - body (which is going to take some jsx as the value)
 // - actions (which is also going to be some jsx)
 // use the Card from MUI to do this.
+// docs here: https://mui.com/material-ui/react-card/
