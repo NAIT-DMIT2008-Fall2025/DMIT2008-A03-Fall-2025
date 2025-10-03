07c857d45723659483fad03350b74e36c69f0a63
Daniel Mouris <dgmouris@gmail.com>
2025-10-03 13:56:11 -0600

    class progress pushed

--- 5-nextjs-mui-component-library-START/component-mui-example/pages/index.js
+++ 5-nextjs-mui-component-library-START/component-mui-example/pages/index.js
@@ -5,7 +5,8 @@ import Container from '@mui/material/Container';
 // a grid: https://mui.com/material-ui/react-grid/
 import Grid from '@mui/material/Grid';
 // a typography component with a title that says MUI example
-
+// docs: https://mui.com/material-ui/react-typography/
+import Typography from '@mui/material/Typography';
 
 // gut the index page
 export default function Home() {
@@ -15,7 +16,13 @@ export default function Home() {
       <Grid container spacing={2}>
         <Grid size={12}>
           {/* we're going to put the title here */}
-          <h1>Home</h1>
+          <Typography
+            variant="h1"
+            component="h1"
+            gutterBottom
+          >
+            Intro to MUI
+          </Typography>
         </Grid>
       </Grid>
     </Container>
