ca08e931081ae4aabdf0045f18a96c1842e282ab
Daniel Mouris <dgmouris@gmail.com>
2025-10-08 14:17:06 -0600

    class progress pushed

--- 6-nextjs-mui-theming-START/mui-theme-example/pages/_app.js
+++ 6-nextjs-mui-theming-START/mui-theme-example/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css';
 
+import { ThemeProvider } from '@mui/material/styles';
+
+import { theme } from '../utils/theme/config';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // we're going import the themeProvider
+  // we're going to wrap our entire component with
+  // that theme provider.
+  return <ThemeProvider theme={{}}>
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
 
 export default MyApp
