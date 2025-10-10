04b9ee65615bdf1f0eb3475da67b37c47d155997
Daniel Mouris <dgmouris@gmail.com>
2025-10-10 14:29:41 -0600

    class progress pushed

--- 7-nextjs-event-state-fundamentals-START/state-events-fundamentals/pages/index.js
+++ 7-nextjs-event-state-fundamentals-START/state-events-fundamentals/pages/index.js
@@ -6,10 +6,22 @@ i want a title with Typography
 that will say "Our TodoList"
 
 */
+import Container from '@mui/material/Container';
+// the file in material will be the name of the component
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+// note: add roboto it'll be a good idea.
+
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid size={12}>
+          <Typography variant={'h2'}>
+            Our TodoList
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
