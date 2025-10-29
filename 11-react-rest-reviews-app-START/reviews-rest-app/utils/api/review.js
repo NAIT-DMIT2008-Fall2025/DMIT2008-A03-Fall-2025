69ae8b92cce376c66f4a404e2514d506674b9756
Daniel Mouris <dgmouris@gmail.com>
2025-10-29 13:34:07 -0600

    class progress pushed

--- 11-react-rest-reviews-app-START/reviews-rest-app/utils/api/review.js
+++ 11-react-rest-reviews-app-START/reviews-rest-app/utils/api/review.js
@@ -1,8 +1,9 @@
 const BASE_URL = 'http://localhost:5000'
 
-export const getReviews = () => {
+export const getReviews = async () => {
   // 1. create a fetch request
   // using the async/await syntax
   // that will return the reviews from the
   // /reviews endpoint on the BASE_URL
+  const response = await fetch(`${BASE_URL}/reviews`)
 }
