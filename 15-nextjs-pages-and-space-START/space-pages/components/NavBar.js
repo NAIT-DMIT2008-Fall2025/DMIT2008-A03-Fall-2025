64f524d45f92b752afac3aaeb0a91d6f70556191
Daniel Mouris <dgmouris@gmail.com>
2025-11-14 14:01:34 -0700

    class progress pushed

--- 15-nextjs-pages-and-space-START/space-pages/components/NavBar.js
+++ 15-nextjs-pages-and-space-START/space-pages/components/NavBar.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography'
@@ -6,12 +7,16 @@ import Typography from '@mui/material/Typography'
 export default function NavBar(props) {
   return <AppBar position="static">
     <Toolbar>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+      <Link href="/">
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Space Agency App
-      </Typography>
-      <Typography variant="h6" component="div" >
-        About
-      </Typography>
+        </Typography>
+      </Link>
+      <Link href="/about">
+        <Typography variant="h6" component="div" >
+          About
+        </Typography>
+      </Link>
     </Toolbar>
   </AppBar>
 }
