ffe70a235565821a282b517f6ec53d33a3054e92
Daniel Mouris <dgmouris@gmail.com>
2025-11-14 14:31:38 -0700

    class progress pushed

--- 15-nextjs-pages-and-space-START/space-pages/pages/agency/[agencyId].js
+++ 15-nextjs-pages-and-space-START/space-pages/pages/agency/[agencyId].js
@@ -1,3 +1,5 @@
+import {useEffect} from 'react'
+
 import {useRouter} from 'next/router'
 
 export default function AgencyPage() {
@@ -8,6 +10,21 @@ export default function AgencyPage() {
   // our file name is [agencyId].js
   // this is where we're gitting this data
   const { agencyId } = router.query
+
+  useEffect(()=> {
+    // you'll need to add this line
+    if (!agencyId) {
+      return
+    }
+
+  }, [])
+
+  // get agency data
+  // import the state
+  // create a stateful variable for agency
+  // handle the loading state
+  // just put the title.
+
 
   return <>
     <h1>Agency {agencyId}</h1>
