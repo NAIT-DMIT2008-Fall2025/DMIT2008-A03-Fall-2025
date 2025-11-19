45061493fe25ba019c12190d59968150e6b4abfe
Daniel Mouris <dgmouris@gmail.com>
2025-11-19 13:41:29 -0700

    class progress pushed

--- 15-nextjs-pages-and-space-START/space-pages/pages/agency/[agencyId].js
+++ 15-nextjs-pages-and-space-START/space-pages/pages/agency/[agencyId].js
@@ -7,6 +7,8 @@ import Typography from '@mui/material/Typography'
 
 import LoadingCircle from '@components/LoadingCircle'
 import NavBar from '@components/NavBar'
+import SimpleDetailsCard from '@components/SimpleDetailsCard'
+
 import { getAgency } from '@utils/api/agencies'
 
 export default function AgencyPage() {
@@ -16,7 +18,7 @@ export default function AgencyPage() {
   const router = useRouter()
 
   // get the agencyId which is the dynamic Path
-  console.log(router)
+  // console.log(router)
   // our file name is [agencyId].js
   // this is where we're gitting this data
   const { agencyId } = router.query
@@ -51,9 +53,45 @@ export default function AgencyPage() {
   return <>
     <NavBar />
     <Container>
-    <Typography variant="h3" component="h1">
+    <Typography variant="h3" component="h1" margin={2}>
       Agency: {agency.name} ({agency.abbrev})
     </Typography>
+    <SimpleDetailsCard
+      title={"Administrator"}
+      description={agency.administrator}
+    />
+    <SimpleDetailsCard
+      title={"Description"}
+      description={agency.description}
+    />
+    <Typography variant="h3" component="h1" margin={2}>
+      SpaceCraft List
+    </Typography>
+    { agency.spacecraft_list.map((spacecraft)=> {
+      return <SimpleDetailsCard
+        key={spacecraft.id}
+        description={spacecraft.name}
+        buttonName={`Go to SpaceCraft Page`}
+        buttonCallback={()=> {
+          // in this call back to navigate to a page that is called
+          // /spacecraft/spacecraftId where spacecraftId is dynamic
+          // in a new file you'll create do the following.
+          // create the page so that it doesn't 404
+          // get the spacecraftId and render it on the page.
+        }}
+
+      />
+    })}
+
+    {/* using the siple details card
+      3 cards
+        1. title: "Description"
+           description: from the data
+        2. title "Administrator"
+           description: from the data
+        3. Create a list of space_craft from the data.
+            you can create these as cards.
+    */}
 
     </Container>
   </>
